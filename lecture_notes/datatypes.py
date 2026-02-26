d62db3e89a72db0600469a3354ee504a21aafcdd
Ojiugo Egeonu <ojiugo_egeonu@berkeley.edu>
2026-02-26 10:35:34 -0800

    moved test.py

---
+++ lecture_notes/datatypes.py
@@ -0,0 +1,22 @@
+#INTRO
+#print("Hello, World!") #DEF: string
+
+#x = "Hello, World!" #assigning text/num to variable
+#print(x)
+#print(type(x))  #type: tell you its DEF
+
+#a = 2.5 #be sure to use different variables
+#print(2.5)  #DEF:float, interger = w/o decimal
+#print(type(a))
+
+#Practice Question
+b = ["Ojiugo", 0,1,2] #DEF: list
+print(b)
+print(type(b))
+
+#Practice Question
+#pwd: prints the folder you're working in
+#ls: lists files/folders inside current folder
+#cat: shows the inside of a file on the command line
+#cd: allows you to move from folder to folder
+#nano: allows you to open up a file and edit
---
+++ lecture_notes/test.py/test.py
@@ -0,0 +1,4 @@
+#File: test.py mathematics
+
+adding = (2 + 5)
+print(adding)
