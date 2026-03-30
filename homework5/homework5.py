2a63c3676bb61d05b1964b0110f03cdea81ef07a
Ojiugo Egeonu <ojiugo_egeonu@berkeley.edu>
2026-03-30 15:17:36 -0700

    done with hw7

---
+++ homework5/homework5.py
@@ -0,0 +1,70 @@
+                                                            # Homework 1 + 2 Review
+# 1. Git vs. GitHub
+# Git = tracks code changes
+# GitHub = stores code online
+
+# 2. Terminal vs. Command Line
+# Terminal = app you use
+# Command Line = where you type commands
+
+# 3. Local vs. Remote Repository
+# Local = on your computer
+# Remote = online (GitHub)
+
+# 4. Version Control
+# Keeps track of changes in files
+
+# 5. Staging Area
+# Where files go before saving (commit)
+
+# 6. git add
+# Prepares files to be saved
+
+# 7. git commit
+# Saves changes
+
+# 8. git push
+# Sends changes online
+
+# 9. git status
+# Shows what’s changed
+
+# 10. git pull
+# Gets latest changes from online
+
+# 11. pwd
+# Shows current folder
+
+# 12. ls
+# Lists files
+
+# 13. cd
+# Moves between folders
+
+# 14. nano
+# Opens text editor
+
+# 15. touch
+# Creates file
+
+# 16. mv
+# Moves or renames file
+
+# 17. rm
+# Deletes file
+
+# 18. cat
+# Shows file contents
+
+                                                        # Directory Tree
+#1. pwd
+#2. ls
+#3. cd ../brianna_repo, git pull
+#4. mv homework.py ../judy_decal/homework/
+#5. cd ../judy_decal/homework
+#6. cat homework.py
+#7. git add . , git commit -m "finished homework", git push
+#8. git pull, git push
+    # Remote repo has new changes that you don’t have locally, push without updating 
+    # She didn’t pull before pushing.
+#9. cd ~/Recent
